ce5651109b5f7c4bc9863b4abd3cb55f899fb312
dora4 <71242257+dora4@users.noreply.github.com>
2023-09-28 09:48:55 +0800

    减少不必要的库依赖

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -25,11 +25,9 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.recyclerview:recyclerview:1.3.1")
     implementation("com.github.dora4:dview-colors:1.0")
-    implementation("io.github.cymchad:BaseRecyclerViewAdapterHelper:3.0.10")
+    api("io.github.cymchad:BaseRecyclerViewAdapterHelper:3.0.10")
 }
 
 afterEvaluate {
@@ -39,7 +37,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = rootProject.project.name
-                version = "1.4"
+                version = "1.5"
             }
         }
     }
